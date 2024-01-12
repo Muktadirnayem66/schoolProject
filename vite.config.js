a0a8293309b785479d0cb857706d100ddb4703e6
Muktadirnayem66 <mmuktadir629@gmail.com>
2024-01-12 17:17:14 +0600

    folder name changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Assignment-1/",
+  base:"/assignment-1/",
   plugins: [react()],
 })
